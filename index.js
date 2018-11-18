392e1de211113561470c3441a3624eac024dd067
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-11-18 17:27:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here:
+function driversWithRevenueOver(arr, num) {
+  return arr.filter(function (el) {
+    return el.revenue > num;
+  });
+}
+
+function driverNamesWithRevenueOver(arr, num) {
+  return driversWithRevenueOver(arr, num)
+  .map(function (el) {
+    return el.name;
+  });
+}
+
+function exactMatch (arr, obj) {
+  return arr.filter(function (el) {
+    let results = false;
+/* why this?*/
+    for (const key in obj) {
+      results = el[key] === obj[key];
+    }
+
+    return results;
+  });
+}
+
+function exactMatchToList(arr, obj) {
+  return exactMatch(arr, obj)
+  .map(function (element) {
+    return element.name;
+  })
+}
